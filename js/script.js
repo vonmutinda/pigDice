d1f63398d0b0ea31a8fa7312f71505867d06f6a5
vonmutinda <maxwellwambua@gmail.com>
2018-05-11 16:39:07 +0300

    javascript objects created

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
-$(document).ready();
+$(document).ready(function(){
+
+    function Dice() {
+        
+    }
+
+
+    let players = {
+        defaults : {
+            playerOne: "von MUTINDA",
+            playerTwo: "Nyamosi"
+        }
+    }
+
+    $('#loadGame').click(function(){
+
+        players.firstPlayer= userOne = $().val();
+         players.secondPlayer = $().val();
+
+    // hide age section display playground
+        $().toggle();
+        $().show();
+
+
+    // load names of players
+    if(firstPlayer!== '' && secondPlayer!=''){
+        $().append();
+        $().append();
+    }else{
+        $().append(defaults.playerOne);
+        $().append(defaults.playerTwo);
+    }
+        
+        
+
+    });
+
+//roll dice
+    $().click(function(){
+        let score = Math.floor((Math.random() * 6) + 1);
+        $().append(score);
+
+        score +=score;
+    });
+
+
+
+
+});
