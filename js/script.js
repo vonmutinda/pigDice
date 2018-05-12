a293f8246835589985e88fc46fc429f33313b6d2
vonmutinda <maxwellwambua@gmail.com>
2018-05-12 18:15:03 +0300

    javascript for player 1 created

--- js/script.js
+++ js/script.js
@@ -1,74 +1,102 @@
 $(document).ready(function(){
 
-    function Dice() {
-        this.win=50;
+// GLOBAL VARIABLES
+let outcome = 0;
 
-    }
 
-    function Players(firstPlayer,secondPlayer){
-        this.firstPlayer=firstPlayer;
-        this.secondPlayer=secondPlayer;
-    }
 
-    // let players = {
-    //     defaults : {
-    //         playerOne: "von MUTINDA",
-    //         playerTwo: "Nyamosi"
-    //     }
-    // }
-
-    $('#loadGame').click(function(){
-
-        let players = new Players($('#playerOne').val(), $('#playerTwo').val());
-
-         // load names of players
-         if (firstPlayer !== '' && secondPlayer != '') {
-             $('#pOne').append(players.firstPlayer);
-             $('#pTwo').append(players.secondPlayer);
-         } else {
-             $('#pOne').append(defaults.playerOne);
-             $('#pTwo').append(defaults.playerTwo);
-         }
-
-    // hide age section display playground
-        $('.login').toggle();
-        $('.ground').show();     
-        
-
-    });
-
-//roll dice
+//GAME
     $('#roll').click(function(){
-        let score = Math.floor((Math.random() * 6) + 1);
+        let play = new Dice();
 
-        if(score==1){
-            alert('Ooops ! You Rolled 1 .');
-            $('#resultOne').remove();
-            $('#turnTwo').fadeIn();
-        }else{
+        let score = parseInt(play.roll());
+
+        console.log(score);
+
+        if (score > 1) {
+            outcome += score;
+
             $('#score').append(score);
-            $('ul#resultOne').append('<li>' + score + '</li>');
-            
+            if (outcome === play.win) {
+                alert('Woooow ! You Win!');
+            }
+        } else {
+            alert('Oops ! You Rolled 1');
+            $('.roll').toggleClass('animating');
+            $('#score').text('');
         }
-        score += score;
-        $('#totalOne').val(score);
-       
+
+        spin(score);
         
-    // clear your score
-        $('#score').text('');
     });
 
+    $
 
-function win(score){
-    
+
+
+// FUNCTIONS ,FACTORIES , CONSTRUCTORS & OBJECTS
+
+function Dice() {
+    this.sides = 6;
+    this.win = 50;
+    this.roll = function roll() {
+        let num = Math.floor((Math.random() * this.sides) + 1);
+        return num;
+    }
 
 }
 
+//spin images 
+function spin(score) {
 
-//animating the roll button
-    $('#roll').click(function(){
-        $('.roll').toggleClass('animating');
-    });
+    if (score === 1) {
+        $('.one').toggleClass('fa-spin');
+    } else if (score === 2) {
+        $('.two').toggleClass('fa-spin');
+    } else if (score === 3) {
+        $('.three').toggleClass('fa-spin');
+    } else if (score === 4) {
+        $('.four').toggleClass('fa-spin');
+    } else if (score === 5) {
+        $('.five').toggleClass('fa-spin');
+    } else {
+        $('.six').toggleClass('fa-spin');
+    }
+
+}
+
+function Players(firstPlayer, secondPlayer) {
+    this.firstPlayer = firstPlayer;
+    this.secondPlayer = secondPlayer;
+    this.theDefaults = {
+        fhirst: 'vonMUTINDA',
+        second: 'bERYLnYAMOSI'
+    }
+}
+
+// EVENT LISTENERS
+
+
+// load playground
+$('#loadGame').click(function () {
+
+    let players = new Players($('#playerOne').val(), $('#playerTwo').val());
+
+    // load names of players
+    if (firstPlayer !== '' && secondPlayer != '') {
+        $('#pOne').append(players.firstPlayer);
+        $('#pTwo').append(players.secondPlayer);
+    } else {
+        $('#pOne').append(players.theDefaults.fhirst);
+        $('#pTwo').append(players.defaults.second);
+    }
+
+    // hide login & display playground
+    $('.login').toggle();
+    $('.ground').show();
+
+
+});
 
 
 });
